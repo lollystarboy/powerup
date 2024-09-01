ba9b54b41023217aae8a954e5b6abde2b80ad3ff
cherneckiy <deneff.max@yandex.ru>
2024-09-01 15:32:21 +0300

    Добавьте файлы проекта.

---
+++ powerup/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace powerup
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+
+            //Результат 2 в 3 степени
+            Console.WriteLine(PowerUp(2, 3));
+        }
+
+        public static int PowerUp(int N, byte pow)
+        {
+            if (pow==0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (pow==1)
+                {
+                    return N;
+                }
+                else
+                {
+                    return N * PowerUp(N, --pow);
+                }
+            }
+        }
+    }
+}
+}
